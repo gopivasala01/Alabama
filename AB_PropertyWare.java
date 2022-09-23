2d6eed5c6dccaa3d5a3eb11c4957a9ebb40d0305
gopivasala01 <49265100+gopivasala01@users.noreply.github.com>
2022-09-23 17:55:21 +0530

    Add files via upload

---
+++ AB_PropertyWare.java
@@ -0,0 +1,39 @@
+package alabama;
+
+public class AB_PropertyWare 
+{
+    public static String commensementDate;
+    public static String expirationDate;
+    public static String proratedRent;
+    public static String proratedRentDate;
+    public static String monthlyRent;
+    public static String monthlyRentDate;
+    public static String adminFee;
+    public static String airFilterFee;
+    public static String earlyTermination;
+    public static String occupants;
+    public static String lateChargeDay;
+    public static String lateChargeFee;
+    public static String proratedPetRent;
+    public static String proratedPetRentDate;
+    public static String petSecurityDeposit;
+    public static String RCDetails;
+    public static String petRent;
+    public static String petRent1;
+    public static String pet1Type;
+    public static String pet2Type;
+    public static String serviceAnimalType;
+    public static String pet1Breed;
+    public static String pet2Breed;
+    public static String serviceAnimalBreed;
+    public static String pet1Weight;
+    public static String pet2Weight;
+    public static String serviceAnimalWeight;
+    public static String petOneTimeNonRefundableFee;
+    public static int countOfTypeWordInText;
+    public static String lateFeeChargeDay;
+    public static String lateFeeAmount;
+    public static String lateFeeChargePerDay;
+    public static String additionalLateCharges;
+    public static String additionalLateChargesLimit;
+}
